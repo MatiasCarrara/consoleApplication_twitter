7125851e5c46c931c25e740737fb003861b0216c
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-16 10:29:39 -0300

    Search Hashtag

--- search.rb
+++ search.rb
@@ -14,14 +14,13 @@ module Twitter
     access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
   end
 
-  def self.here(raul)
+  def self.search(hashtag)
     access_token = prepare_access_token(@access_token, @access_token_secret)
-    response = access_token.request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{raul}&count=10").body
+    response = access_token.request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{hashtag}&count=10").body
     var = JSON.parse(response)['statuses']
     text = []
-    var.each { |lol| text.push(lol['text']) }
-    print text
-  # puts JSON.pretty_generate(var)
-
+    var.each { |save| text.push(save['text']) }
+    text
   end
 end
+Twitter.search("Penarol")
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -2,53 +2,42 @@ require_relative "../search.rb"
 require "webmock/rspec"
 
 describe Twitter do
+
   describe ".prepare_access_token" do
-
     subject { Twitter.prepare_access_token(token, token_secret ) }
-      let(:token) { "946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu" }
-      let(:token_secret) { "awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9" }
-
-      context "token" do
-          it '' do
-           expect(subject.token).to eq(token)
-          end
+    let(:token) { "946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu" }
+    let(:token_secret) { "awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9" }
+    context "token" do
+      it '' do
+        expect(subject.token).to eq(token)
       end
-      context "token_secret" do
-          it '' do
-            expect(subject.secret).to eq(token_secret)
-          end
+    end
+    context "token_secret" do
+      it '' do
+        expect(subject.secret).to eq(token_secret)
       end
+    end
   end
 
-  describe ".here" do
-
-    subject { Twitter.here(tag) }
-    context "buscar" do
-
-        let(:text) { "mi name es Matias" }
-        let(:body) do  {
-          statuses: [
-            {
-              created_at: "Thu Jan 11 14:11:01 +0000 2018",
-              id: 951456415025172481,
-              id_str: "951456415025172481",
-              text: text,
-              }]
-            }
+  describe ".search" do
+    subject { Twitter.search(hashtag) }
+    context "search a hashtag" do
+      let(:text) { "i using facebook" }
+      let(:body) do  {
+        statuses: [{
+            created_at: "Tue Jan 16 12:26:53 +0000 2018",
+            id: 953242151655280640,
+            id_str: "953242151655280640",
+            text: text,
+            }] }
           end
-
-          let(:tag) { "Penarol" }
-          before do
-            stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{tag}&count=10").to_return(body: JSON(body))
-         end
-
-         # subject { Twitter.here(tag) }
-         # it { is_expected.to eq(text) }
-
-        it 'hola' do
-          expect(subject).to eq(text)
-        end
-
+      let(:hashtag) { "pdpdkowejfin" }
+      before do
+        stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{hashtag}&count=10").to_return(body: JSON(body))
+      end
+      it 'return the tweet text' do
+        expect(subject).to eq([text])
+      end
     end
   end
 end
