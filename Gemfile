6e9ce07143ad6654b9522098e677d73d8f95043f
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-18 09:40:08 -0300

    Search Hashtag

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 # Gemfile
 source 'https://rubygems.org'
 
+gem 'dotenv'
 gem 'rspec'
 gem 'webmock'
--- search.rb
+++ search.rb
@@ -1,20 +1,16 @@
 module Twitter
   require 'json'
   require 'oauth'
-
-  KEY = 'AA58vqAUmVe1T5VLVX1kS5zJP'.freeze
-  SECRET = '571yBxW3VQIkO62i2Mrn58S8hCAaKl5RbpcC9MEzfByAchDxKg'.freeze
-  TOKEN = '946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu'.freeze
-  TOKEN_SECRET = 'awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9'.freeze
+  require 'dotenv'
 
   def self.prepare_access_token(oauth_token, oauth_token_secret)
-    consumer = OAuth::Consumer.new(KEY, SECRET, site: 'https://ads.api.twitter.com')
+    consumer = OAuth::Consumer.new(ENV['KEY'], ENV['SECRET'], site: 'https://ads.api.twitter.com')
     token_hash = { oauth_token: oauth_token, oauth_token_secret: oauth_token_secret }
     OAuth::AccessToken.from_hash(consumer, token_hash)
   end
 
   def self.search(hashtag)
-    access_token = prepare_access_token(TOKEN, TOKEN_SECRET)
+    access_token = prepare_access_token(ENV['TOKEN'], ENV['TOKEN_SECRET'])
     response = access_token.request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{hashtag}&count=10").body
     back = JSON.parse(response)['statuses']
     back.map { |save| save['text'] }
