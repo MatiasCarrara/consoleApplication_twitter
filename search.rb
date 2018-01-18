48bd20c7dc3050197240c3e2b9e34ca05de82682
MatiasCarrara <matiascarrara@MatiasCarraras-MacBook-Pro.local>
2018-01-18 08:35:25 -0300

    Search Hashtag

--- search.rb
+++ search.rb
@@ -1,17 +1,16 @@
 module Twitter
-  require "json"
-  require "oauth"
+  require 'json'
+  require 'oauth'
 
-  KEY        = "AA58vqAUmVe1T5VLVX1kS5zJP"
-  SECRET     = "571yBxW3VQIkO62i2Mrn58S8hCAaKl5RbpcC9MEzfByAchDxKg"
-  TOKEN        = "946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu"
-  TOKEN_SECRET = "awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9"
-
+  KEY = 'AA58vqAUmVe1T5VLVX1kS5zJP'.freeze
+  SECRET = '571yBxW3VQIkO62i2Mrn58S8hCAaKl5RbpcC9MEzfByAchDxKg'.freeze
+  TOKEN = '946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu'.freeze
+  TOKEN_SECRET = 'awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9'.freeze
 
   def self.prepare_access_token(oauth_token, oauth_token_secret)
-    consumer = OAuth::Consumer.new(KEY, SECRET, { site:  "https://ads.api.twitter.com"})
-    token_hash = { oauth_token:  oauth_token, oauth_token_secret:  oauth_token_secret }
-    access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
+    consumer = OAuth::Consumer.new(KEY, SECRET, site: 'https://ads.api.twitter.com')
+    token_hash = { oauth_token: oauth_token, oauth_token_secret: oauth_token_secret }
+    OAuth::AccessToken.from_hash(consumer, token_hash)
   end
 
   def self.search(hashtag)
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,37 +1,40 @@
-require_relative "../search.rb"
-require "webmock/rspec"
+require_relative '../search.rb'
+require 'webmock/rspec'
 
 describe Twitter do
-
-  describe ".prepare_access_token" do
-    subject { Twitter.prepare_access_token(token, token_secret ) }
-    let(:token) { "946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu" }
-    let(:token_secret) { "awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9" }
-    context "access token" do
-      it '' do
+  describe '.prepare_access_token' do
+    subject { Twitter.prepare_access_token(token, token_secret) }
+    let(:token) { '946407467315073024-4VXdadSPr1N4tNtCqBU44Wzc4bcjWxu' }
+    let(:token_secret) { 'awImllz0NQD1sfMu0UQzYiOBCKedOZNVFDolPTbESUzM9' }
+    context 'access token' do
+      it 'return the access token' do
         expect(subject.token).to eq(token)
       end
     end
-    context "access token secret" do
-      it '' do
+    context 'access token secret' do
+      it 'return access to the secret token' do
         expect(subject.secret).to eq(token_secret)
       end
     end
   end
 
-  describe ".search" do
+  describe '.search' do
     subject { Twitter.search(hashtag) }
-    context "search a hashtag" do
-      let(:text) { "i using facebook" }
-      let(:body) do  {
-        statuses: [{
-            created_at: "Tue Jan 16 12:26:53 +0000 2018",
-            id: 953242151655280640,
-            id_str: "953242151655280640",
-            text: text,
-            }] }
-          end
-      let(:hashtag) { "pdpdkowejfin" }
+    context 'search a hashtag' do
+      let(:text) { 'i using facebook' }
+      let(:body) do
+        {
+          statuses: [
+            {
+              created_at: 'Tue Jan 16 12:26:53 +0000 2018',
+              id: 953_242_151_655_280_640,
+              id_str: '953242151655280640',
+              text: text
+            }
+          ]
+        }
+      end
+      let(:hashtag) { 'pdpdkowejfin' }
       before do
         stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{hashtag}&count=10").to_return(body: JSON(body))
       end
