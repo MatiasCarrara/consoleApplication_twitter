b0487f2a9fcfa203b134f98317022ae6089c012e
MatiasCarrara <MatiasCarrara@users.noreply.github.com>
2018-01-18 11:29:10 -0300

    Search (#1)
    * nada
    
    * nada
    
    * nada
    
    * Search Hashtag
    
    * nada
    
    * Search Hashtag
    
    * Search Hashtag
    
    * Search Hashtag
    
    * Search Hashtag
    
    * Search Hashtag

---
+++ search.rb
@@ -0,0 +1,20 @@
+module Twitter
+  require 'json'
+  require 'oauth'
+  require 'dotenv'
+
+  Dotenv.load('.env')
+
+  def self.prepare_access_token(oauth_token, oauth_token_secret)
+    consumer = OAuth::Consumer.new(ENV['KEY'], ENV['SECRET'], site: 'https://ads.api.twitter.com')
+    token_hash = { oauth_token: oauth_token, oauth_token_secret: oauth_token_secret }
+    OAuth::AccessToken.from_hash(consumer, token_hash)
+  end
+
+  def self.search(hashtag)
+    access_token = prepare_access_token(ENV['TOKEN'], ENV['TOKEN_SECRET'])
+    response = access_token.request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{hashtag}&count=10").body
+    back = JSON.parse(response)['statuses']
+    back.map { |save| save['text'] }
+  end
+end
